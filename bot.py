c1afa60ed758bcd8998479dc6149b74879b82e52
Aravindhan <aravindhan.ct19@bitsathy.ac.in>
2023-06-23 22:01:07 +0530

    Fixed error in sending long message

--- bot.py
+++ bot.py
@@ -5,5 +5,9 @@ token = os.environ.get('BOT_TOKEN')
 botApp = Bot(token=token)
 
 
-async def send_message(chat_id, text):
-    await botApp.send_message(chat_id, text)
+async def send_message(chat_id, message):
+    if len(message) > 4095:
+        for x in range(0, len(message), 4095):
+            await botApp.send_message(message, text=message[x:x+4095])
+    else:
+        await botApp.send_message(chat_id, message)

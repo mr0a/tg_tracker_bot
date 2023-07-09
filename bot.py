550624b760cc1f93d34746dfbbce658b2ce1778c
Aravindhan A <aravindhan.asaithambi@cdw.com>
2023-07-09 23:13:00 +0530

    refactored bot client

--- bot.py
+++ bot.py
@@ -2,12 +2,10 @@ from aiogram import Bot
 import os
 
 token = os.environ.get('BOT_TOKEN')
+if not token:
+    raise Exception("Bot token is not set!")
 botApp = Bot(token=token)
 
 
-async def send_message(chat_id, message):
-    if len(message) > 4095:
-        for x in range(0, len(message), 4095):
-            await botApp.send_message(chat_id, text=message[x:x+4095])
-    else:
-        await botApp.send_message(chat_id, message)
+def getBotClient():
+    return botApp

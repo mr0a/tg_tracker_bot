b0178fcdec73cdfd24a8e561257373bd35d1b2df
Aravindhan A <aravindhan.asaithambi@cdw.com>
2023-07-09 23:14:02 +0530

    Added channel tracker model

--- models.py
+++ models.py
@@ -19,3 +19,15 @@ class Trackers(Model):
 
     def __str__(self):
         return f"Trackers(chat_id={self.user}, token_name='{self.token_name}', time_interval='{self.time_interval}', price_tick={self.price_tick})"
+
+
+class ChannelTracker(Model):
+    user = fields.ForeignKeyField('models.User', related_name='channelTracker')
+    channel_id = fields.CharField(max_length=255)
+    joined = fields.BooleanField(default=False)
+    pattern = fields.CharField(max_length=255, null=True)
+    cron_interval = fields.CharField(max_length=255)
+    last_tracked_id = fields.BigIntField(null=True)
+
+    def __str__(self) -> str:
+        return f"ChannelTracker(user={self.user}, channel_id={self.channel_id}, joined={self.joined}, pattern={self.pattern}, time_interval={self.cron_interval})"
